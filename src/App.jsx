af19864a62309f45f5adca11e75d8a578d2533cd
Aung-Myat-Oo <aungmyatoo2025@gmail.com>
2023-12-13 21:09:36 +0630

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import ContextProvider from "./context/ContextProvider";
 import "./App.css";
 import Home from "./home/Home";
-import { HashRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Movie from "./movie/Movie";
 import FavoriteMovieList from "./favorite/FavoriteMovieList";
 import PageNotFound from "./page_not_found/PageNotFound";
@@ -10,14 +14,14 @@ import PageNotFound from "./page_not_found/PageNotFound";
 const App = () => {
   return (
     <ContextProvider>
-      <BrowserRouter basename="/movie-app/">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<Movie />} />
-          <Route path="/favorite" element={<FavoriteMovieList />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<Movie />} />
+            <Route path="/favorite" element={<FavoriteMovieList />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Router>
     </ContextProvider>
   );
 };
